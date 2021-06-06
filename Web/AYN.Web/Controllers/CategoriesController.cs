b33e80ecc72fd63e1a13170047f3c8642f064643
george <alienguymc@gmail.com>
2021-06-06 17:59:31 +0300

    implement create category for admin

---
+++ Web/AYN.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+
+using AYN.Common;
+using AYN.Services.Data;
+using AYN.Web.ViewModels.Categories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYN.Web.Controllers
+{
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoriesService categoriesService;
+        private readonly IWebHostEnvironment environment;
+
+        public CategoriesController(
+            ICategoriesService categoriesService,
+            IWebHostEnvironment environment)
+        {
+            this.categoriesService = categoriesService;
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var wwwrootPath = this.environment
+                .WebRootPath;
+
+            await this.categoriesService.CreateAsync(input, wwwrootPath);
+
+            return this.Redirect("/");
+        }
+    }
+}
