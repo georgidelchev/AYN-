a1e9b3c681052d2af592e39bc6986b368dc1a6bd
george <alienguymc@gmail.com>
2021-06-24 22:07:48 +0300

    working on user posts

---
+++ Web/AYN.Web/ViewComponents/GetAllUserPostsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYN.Web.ViewComponents
+{
+    public class GetAllUserPostsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return this.View();
+        }
+    }
+}
