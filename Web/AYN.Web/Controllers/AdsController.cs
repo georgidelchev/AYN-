2546e70ba2db29721780acef856c8e7ccd41a5b2
george <alienguymc@gmail.com>
2021-07-05 00:15:26 +0300

    add

--- Web/AYN.Web/Controllers/AdsController.cs
+++ Web/AYN.Web/Controllers/AdsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AYN.Web.Controllers
 {
+    [Authorize]
     public class AdsController : Controller
     {
         private readonly ICategoriesService categoriesService;
@@ -38,7 +40,6 @@ namespace AYN.Web.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public async Task<IActionResult> Create()
         {
             var viewModel = new CreateAdInputModel()
@@ -46,26 +47,19 @@ namespace AYN.Web.Controllers
                 Categories = await this.categoriesService.GetAllAsKeyValuePairsAsync(),
                 Towns = await this.townsService.GetAllAsKeyValuePairsAsync(),
             };
-
             return this.View(viewModel);
         }
 
         [HttpPost]
-        [Authorize]
         public async Task<IActionResult> Create(CreateAdInputModel input)
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            var wwwrootPath = this.environment
-                .WebRootPath;
-
-            await this.adsService.CreateAsync(input, userId, wwwrootPath);
-
+            await this.adsService.CreateAsync(input, userId, this.environment.WebRootPath);
             return this.Redirect("/");
         }
 
         [HttpGet]
-        [Authorize]
         public async Task<IActionResult> All(
             string search,
             string town,
@@ -73,50 +67,60 @@ namespace AYN.Web.Controllers
             string orderBy = "createdOnDesc",
             int id = 1)
         {
-            var ads = await this.adsService.GetAllAsync<GetAdsViewModel>(search, orderBy, categoryId);
-
-            var itemsPerPage = 12;
-
-            var viewModel = new ListAllAdsViewModel()
+            try
             {
-                Count = ads.Count(),
-                ItemsPerPage = itemsPerPage,
-                AllFromSearch = ads.Skip((id - 1) * itemsPerPage).Take(itemsPerPage),
-                PageNumber = id,
-                OrderBy = orderBy,
-                Town = town,
-                CategoryId = categoryId,
-                Search = search,
-                TotalResults = ads.Count(),
-                AllCategoriesWithAllSubCategories = new Dictionary<CategoryViewModel, List<SubCategoryViewModel>>(),
-            };
+                var ads = await this.adsService
+                    .GetAllAsync<GetAdsViewModel>(search, orderBy, categoryId);
 
-            var categories = this.categoriesService
-                .GetAll<CategoryViewModel>();
+                var itemsPerPage = 12;
 
-            foreach (var category in categories)
-            {
-                var subCategories = await this.subCategoriesService
-                    .GetAllByCategoryId<SubCategoryViewModel>(category.Id)
-                    .ToListAsync();
-
-                var categoryViewModel = new CategoryViewModel()
+                var viewModel = new ListAllAdsViewModel()
                 {
-                    Id = category.Id,
-                    Name = category.Name,
-                    PictureExtension = category.PictureExtension,
+                    Count = ads.Count(),
+                    ItemsPerPage = itemsPerPage,
+                    AllFromSearch = ads.Skip((id - 1) * itemsPerPage).Take(itemsPerPage),
+                    PageNumber = id,
+                    OrderBy = orderBy,
+                    Town = town,
+                    CategoryId = categoryId,
+                    Search = search,
+                    TotalResults = ads.Count(),
+                    AllCategoriesWithAllSubCategories = new Dictionary<CategoryViewModel, List<SubCategoryViewModel>>(),
                 };
 
-                viewModel.AllCategoriesWithAllSubCategories
-                    .Add(categoryViewModel, subCategories);
-            }
+                var categories = this.categoriesService
+                    .GetAll<CategoryViewModel>();
 
-            return this.View(viewModel);
+                foreach (var category in categories)
+                {
+                    var subCategories = await this.subCategoriesService
+                        .GetAllByCategoryId<SubCategoryViewModel>(category.Id)
+                        .ToListAsync();
+
+                    var categoryViewModel = new CategoryViewModel()
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                        PictureExtension = category.PictureExtension,
+                    };
+
+                    viewModel.AllCategoriesWithAllSubCategories
+                        .Add(categoryViewModel, subCategories);
+                }
+
+                return this.View(viewModel);
+            }
+            catch (Exception e)
+            {
+                return this.View();
+            }
         }
 
         [HttpGet]
-        [Authorize]
         public async Task<IActionResult> Details(string id)
-            => this.View(await this.adsService.GetDetails<GetDetailsViewModel>(id));
+        {
+            var viewModel = await this.adsService.GetDetails<GetDetailsViewModel>(id);
+            return this.View(viewModel);
+        }
     }
 }
