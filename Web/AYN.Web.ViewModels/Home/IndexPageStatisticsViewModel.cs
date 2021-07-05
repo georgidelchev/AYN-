2e4bfe4a644a0e972b7e8470c97c35e855717e07
george <alienguymc@gmail.com>
2021-07-06 00:07:24 +0300

    implement home page statistics about the active users and active ads in the platform

---
+++ Web/AYN.Web.ViewModels/Home/IndexPageStatisticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AYN.Web.ViewModels.Home
+{
+    public class IndexPageStatisticsViewModel
+    {
+        public int UsersCount { get; set; }
+
+        public int AdsCount { get; set; }
+    }
+}
---
+++ Web/AYN.Web/ViewComponents/GetHomePageStatisticsViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using AYN.Services.Data.Interfaces;
+using AYN.Web.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYN.Web.ViewComponents
+{
+    public class GetHomePageStatisticsViewComponent : ViewComponent
+    {
+        private readonly IAdsService adsService;
+        private readonly IUsersService usersService;
+
+        public GetHomePageStatisticsViewComponent(
+            IAdsService adsService,
+            IUsersService usersService)
+        {
+            this.adsService = adsService;
+            this.usersService = usersService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewModel = new IndexPageStatisticsViewModel()
+            {
+                AdsCount = this.adsService.GetCounts().Item2,
+                UsersCount = this.usersService.GetCounts().Item3,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
