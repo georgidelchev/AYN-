3bb1df01654f193474b2919d2069f47e3a95feaa
george <alienguymc@gmail.com>
2021-07-26 22:34:07 +0300

    add

---
+++ Web/AYN.Web.ViewModels/Users/GetFollowersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AYN.Data.Models;
+using AYN.Services.Mapping;
+
+namespace AYN.Web.ViewModels.Users
+{
+    public class GetFollowersViewModel : IMapFrom<ApplicationUser>
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AvatarExtension { get; set; }
+    }
+}
