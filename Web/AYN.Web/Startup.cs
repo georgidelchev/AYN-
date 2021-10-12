410f9a9ea7799fcba16399b3cbb10fd10543fdcd
Georgi Delchev <alienguymc@gmail.com>
2021-10-12 17:55:43 +0300

    test

--- Web/AYN.Web/Startup.cs
+++ Web/AYN.Web/Startup.cs
@@ -18,7 +18,6 @@ using CloudinaryDotNet;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
