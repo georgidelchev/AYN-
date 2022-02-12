0ec02843c2c6fac4ed85452bc9baad5e65a9b35c
george <alienguymc@gmail.com>
2022-02-12 14:44:17 +0200

    integrate tinymce

---
+++ Web/AYN.Web/wwwroot/lib/tinymce/plugins/emoticons/index.min.js
@@ -0,0 +1,11 @@
+/**
+ * Skipped minification because the original files appears to be already minified.
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+// Exports the "emoticons" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/emoticons')
+//   ES2015:
+//     import 'tinymce/plugins/emoticons'
+require('./plugin.js');
